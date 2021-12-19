b808136cfa292a5d25cfd6ca3fccd0a8c2089f1f
AdrianAmerico <1fnadrian1@gmail.com>
2021-12-19 19:27:39 -0300

    redux saga

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    "react-app",
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["react", "import", "jsx-a11y"],
-  rules: {
-    "react/jsx-filename-extension": [
-      "error",
-      {
-        extensions: [".tsx"],
-      },
+    extends: [
+      "react-app",
+      // "airbnb",
+      "plugin:@typescript-eslint/recommended"
     ],
-    "import/prefer-default-export": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
-  },
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
     },
-    "import/resolver": {
-      typescript: {},
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: "module",
     },
-  },
-};
+    plugins: ["react", "import", "jsx-a11y"],
+    rules: {
+      "react/jsx-filename-extension": [
+        "error",
+        {
+          extensions: [".tsx"],
+        },
+      ],
+      "import/prefer-default-export": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+    },
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+  };
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import * as React from "react";
+import { Provider } from "react-redux";
+import RepositoryList from "./components/RepositoryList";
+import store from "./store";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Provider store={store}>
+      <RepositoryList />
+    </Provider>
+  );
 }
 
 export default App;
